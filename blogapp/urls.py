af2a6c2e5950a8de6f0c59127e205f2305de079b
mcgalih <mcgalihramdhan@gmail.com>
2024-01-27 14:38:26 +0700

    change home path, add static file

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
-from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('login', include('users.urls'))
 ]
